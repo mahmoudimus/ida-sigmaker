2981a465d048e5ced9da149a1fbf7b792f36e1cf
Mahmoud Abdelkader <mabdelkader@gmail.com>
2025-08-24 21:39:07 -0700

    Add ida-plugin.json and version

--- src/sigmaker.py
+++ src/sigmaker.py
@@ -27,7 +27,7 @@ import idaapi
 import idc
 
 __author__ = "mahmoudimus"
-__version__ = "1.1.2"
+__version__ = "1.3.0"
 
 PLUGIN_NAME: str = "Signature Maker (py)"
 PLUGIN_VERSION: str = __version__

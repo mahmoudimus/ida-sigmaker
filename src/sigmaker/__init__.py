6fa785a3160c6988770d53da8d655c29b70e2735
Mahmoud Abdelkader <mabdelkader@gmail.com>
2026-05-27 08:47:06 -0700

    chore: bump version to 1.7.0
    Cuts the 1.7.0 release covering the wait-box cancel UX (#24) and
    opt-in partial-on-cancel output (#25). See CHANGELOG.md for the
    full set of user-visible and internal changes.

--- src/sigmaker/__init__.py
+++ src/sigmaker/__init__.py
@@ -25,7 +25,7 @@ import idaapi
 import idc
 
 __author__ = "mahmoudimus"
-__version__ = "1.6.0"
+__version__ = "1.7.0"
 
 PLUGIN_NAME: str = "Signature Maker (py)"
 PLUGIN_VERSION: str = __version__
